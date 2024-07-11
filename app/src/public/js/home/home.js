89b02a1c9422bc2626c0ba7f9ba55fe29b207a55
paw102 <paw1023021@gmail.com>
2024-07-11 14:45:33 +0900

    로그인 기능 중 obj 값 입력해보기

--- app/src/public/js/home/home.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-
-console.log("hello");
-console.log("bye");
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
