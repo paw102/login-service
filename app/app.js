932276da5a1eba75f5bbfe61b0c88aacf5edba65
paw102 <paw1023021@gmail.com>
2024-07-11 14:20:31 +0900

    프론트와 연결

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/",home); // use : middle ware 등록해주는 메서드
 
